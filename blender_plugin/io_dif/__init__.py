0915679c722c9cfbca1e93b317a532118caf1e91
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2020-12-22 17:24:40 +0530

    Version bump

--- blender_plugin/io_dif/__init__.py
+++ blender_plugin/io_dif/__init__.py
@@ -39,7 +39,7 @@ bl_info = {
     "author": "RandomityGuy",
     "description": "Dif import and export plguin for blender",
     "blender": (2, 80, 0),
-    "version": (1, 1, 0),
+    "version": (1, 1, 1),
     "location": "File > Import-Export",
     "warning": "",
     "category": "Import-Export",
