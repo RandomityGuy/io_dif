2b9fb92f673104fb76cb3c1e37507d18fdf0c57a
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2022-02-17 22:50:30 +0530

    Fix broken plane export

--- blender_plugin/io_dif/__init__.py
+++ blender_plugin/io_dif/__init__.py
@@ -40,7 +40,7 @@ bl_info = {
     "author": "RandomityGuy",
     "description": "Dif import and export plugin for blender",
     "blender": (2, 80, 0),
-    "version": (1, 1, 9),
+    "version": (1, 1, 10),
     "location": "File > Import-Export",
     "warning": "",
     "category": "Import-Export",
