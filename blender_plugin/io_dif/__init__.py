a7eaccac8e06743b8715865efc8fe2a6d05b2390
KeppyMarbles <quentin1.is.cool@gmail.com>
2025-10-12 21:23:13 -0500

    inc version

--- blender_plugin/io_dif/__init__.py
+++ blender_plugin/io_dif/__init__.py
@@ -45,7 +45,7 @@ bl_info = {
     "author": "RandomityGuy",
     "description": "Dif import and export plugin for blender",
     "blender": (2, 80, 0),
-    "version": (1, 3, 1),
+    "version": (1, 3, 3),
     "location": "File > Import-Export",
     "warning": "",
     "category": "Import-Export",
